30c41345f228bcd81424df93b0e51f1516ad4dde
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2021-07-06 10:18:22 +0530

    Update settings.py

--- QuizApp/QuizApp/settings.py
+++ QuizApp/QuizApp/settings.py
@@ -143,7 +143,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+#STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+STATIC_ROOT = BASE_DIR /'/static'
 
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
