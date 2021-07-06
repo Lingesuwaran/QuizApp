1130ca1aa7a09dd19a81ce6e6d0a3aa556f7dcf6
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2021-07-06 10:09:07 +0530

    Update settings.py

--- QuizApp/QuizApp/settings.py
+++ QuizApp/QuizApp/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'QuizApp.urls'
 
